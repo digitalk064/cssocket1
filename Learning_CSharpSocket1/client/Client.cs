16d1589ae2999c23ac415eaf3878a5ee8d80add7
digitalk064 <nguynkhanh.lv@gmail.com>
2019-10-08 21:51:02 +0800

    Initial Commit. Only Synchronous for now

---
+++ Learning_CSharpSocket1/client/Client.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace client
+{
+    class Client //Synchronous client, async is much harder 
+    {
+        public static int Main(string[] args)
+        {
+            RunClient();
+            Console.WriteLine("Done! Press any key to exit");
+            Console.ReadKey();
+            return 0;
+        }
+
+        static void RunClient()
+        {
+            byte[] bytes = new byte[1024]; //Bytes received
+            try
+            {
+                //Most of the code here is copied from Microsoft lol
+                //Get the remote endpoint (in this case, ourselves) for the socket
+                IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
+                IPAddress ipAddr = ipHost.AddressList[0];
+                //Does this work?
+                //IPAddress ipAddr = Dns.GetHostAddresses(Dns.GetHostName())[0];
+                IPEndPoint remoteEP = new IPEndPoint(ipAddr, 11000); //Use port 11000 just cause
+
+                //Create TCP/IP socket
+                Socket sender = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+                //Now we connect
+                try //There's never enough try-catch
+                {
+                    sender.Connect(remoteEP); //Connect to the destination
+                    Console.WriteLine("Socket connected to: {0}", sender.RemoteEndPoint.ToString());
+
+                    //Encode message
+                    string sMsg = DateTime.Now.ToString();
+                    byte[] msg = Encoding.ASCII.GetBytes(sMsg + "<EOF>");
+
+                    //Send the data through the socket
+                    int bytesSent = sender.Send(msg);
+                    
+                    //Receive the response
+                    int bytesRec = sender.Receive(bytes);
+                    Console.WriteLine("Incoming msg: {0}",
+                        Encoding.ASCII.GetString(bytes, 0, bytesRec));
+
+                    //Release the socket
+                    sender.Shutdown(SocketShutdown.Both);
+                    sender.Close();
+
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine("Inner catch: {0}", e.ToString());
+                }
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Outer catch: {0}", e.ToString());
+            }
+        }
+    }
+
+}
---
+++ Learning_CSharpSocket1/server/Server.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace server
+{
+    class Server
+    {
+        public static int Main(string[] args)
+        {
+            StartListening();
+            return 0;
+        }
+
+        public static string data = null; //Incoming data
+
+        static void StartListening()
+        {
+            byte[] bytes = new byte[1024]; //Incoming bytes
+
+            //Interesting, no try-catch needed here?
+
+            //Establish the local endpoint for the socket 
+            IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddress = ipHost.AddressList[0];
+            IPEndPoint localEP = new IPEndPoint(ipAddress, 11000);
+
+            //Create TCP/IP socket
+            Socket listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            //Now bind the socket to the local endpoint and start listening
+            try
+            {
+                listener.Bind(localEP);
+                listener.Listen(10);
+
+                //Start listening 
+                while (true)
+                {
+                    Console.WriteLine("Waiting for connection. Program will hang");
+                    //bruh there's another listener
+                    Socket handler = listener.Accept();
+                    data = null;
+
+                    //Process incoming connection
+                    while(true) //Wait why doesn't the client have this loop?
+                    {
+                        int bytesRec = handler.Receive(bytes);
+                        Console.WriteLine("Bytes fragment received");
+                        data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                        //Th is wil l add up th e mes sage s slo w ly
+                        if(data.IndexOf("<EOF>") > -1)
+                        {
+                            break; //Stop expecting message
+                        }
+                    }
+
+                    Console.WriteLine("Final message: {0}", data);
+
+                    //Send our own message back
+                    byte[] msg = Encoding.ASCII.GetBytes("Right back at ya buckaroo"); //Compose it
+                    handler.Send(msg); //Send it
+
+                    //i sleep now
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Outer error: {0}", e.ToString());
+            }
+
+        }
+
+    }
+}
